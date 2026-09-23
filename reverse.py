ac9e6e9f6cda4cff345d5a2a05f451bba61a1cda
Vishnu Dev <vdev8917@gmail.com>
2026-09-23 22:05:40 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,18 @@
+# # num=7536
+# # sum=0
+# # while num>0:
+# #     b = num%10
+# #     sum = sum+ b
+# #     num = num // 10
+
+# # print(sum)
+
+
+# num = 765
+# rev = 0
+# while num>0:
+#     b = num%10
+#     rev = rev*10+b
+    
+
+
