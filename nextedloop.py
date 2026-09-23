ae38ab8e250bc95110d78616bf78df68e381505b
Vishnu Dev <vdev8917@gmail.com>
2026-09-23 22:03:38 +0530

    Add files via upload

---
+++ nextedloop.py
@@ -0,0 +1,30 @@
+ # # for i in range(1,6):
+ # #     for j in range(1,i+1):
+ # #         print("*",end="")
+ # #     print()
+
+
+ # # for i in range(5,0,-1):
+ # #     for j in range(1,i+1):
+ # #         print("*",end="")
+ # #     print()
+
+# # for i in range(1,6):
+# #     for j in range(6-i):
+# #         print(" ",end="")
+# #     for k in range(1,i+1):
+# #             print("* ",end="")
+
+# #     print()
+
+# for i in range(1,9):
+#     for j in range(1,9):
+#         if (i+j) % 2==0:
+#             print("w",end=" ")
+#         else:
+#             print("b",end=" ")
+#     print( )
+
+
+
+
