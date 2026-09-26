dd0be41cc3852a78d04d28092c9d4a9baf5a8481
Vishnu Dev <vdev8917@gmail.com>
2026-09-26 21:10:18 +0530

    Add files via upload

---
+++ python/list of index.py
@@ -0,0 +1,73 @@
+# # index listing
+
+# # name = [11,22,33,44,55]
+# # b = [21,22,23,24,25]
+
+# # for i in range(len(name)):
+# #     print(i,name[i])
+
+
+# data = "vishnudev" 
+# vowels = "aeiou"
+
+# for i in range(len(vowels)):
+#     if data[i] in vowels:
+
+#       print(f"vowels {data[i]} at indexed {i}")      
+
+
+
+# create a list of even  numbers and numbers from first numbers
+
+# listodd = []
+# listeven = []
+# for i in range(0,101):
+#     if i%2==0:
+#         listodd.append(i)
+#     else:
+#         listeven.append(i)
+
+# print(listodd)
+# print(listeven)
+
+#check if the numbers are positive or negative or zero from a given list
+
+# z = [11,22,33,-8,0]
+# for i in range z :
+#     if i>0:
+#         print(i,"positive number")
+#     elif i<0:
+#         print(i,"negative number")
+#     else:
+#         print(i,"zero")
+
+
+# data = [1,2,3,4,5,5,4,3,6,7,8,9,7]
+# real = []
+# for i in range(len(data)):
+#     if i not in real:
+#         real.append(i)
+# print(real)
+
+
+
+
+#find the largest number
+
+# a = [10,-1,0,444,333,789,]
+# largest=0
+# smallest=0
+# for i in a:
+#     if i>largest:
+#         largest=i
+#     if i<smallest:
+#         smallest=i
+
+# print(largest,smallest)
+    
+
+
+
+    
+
+        
