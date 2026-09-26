1dfbfddfa4443bb9ba1d60ebc8132130534cf03a
Vishnu Dev <vdev8917@gmail.com>
2026-09-26 21:12:19 +0530

    Add files via upload

---
+++ python/set n dict.py
@@ -0,0 +1,96 @@
+# set{}
+# collection of data
+# unorderd
+# mutuable
+# unindex
+# does not allow duplicate values
+
+# a = {1,2,3,4,5,4,5,6}
+# b = {1,2,3,4,5,4,5,6}
+
+# print(a.union(b))
+# print(a|b)
+# print(a.intersection(b))
+# print(a&b)
+# print(a+b)
+
+# fruits = {'apple','mango','pineapple','watermelon'}
+# for i in fruits:
+#     print(i)
+
+
+# DICTIONARY KEY:VAlUE PAIR COLLECTION OF DATA
+
+# dict x Index
+# mutuable
+
+
+# data = {"name":"vishnu", "age":21, "location":"kochi"} 
+# print(data)
+# data["email"]="vishnu@gmail.com"
+# print(data)
+# data["name"]="gowri"
+# print(data)
+
+
+#OPERATIONS
+# data = {"name":"vishnu", "age":"21", "location":"kochi"} 
+# print(data["name"])
+# print(data.get("name"))
+
+# data.update({"age":"mohan"})
+
+# data.pop("name")
+# print(data)
+
+# data = {"name":"vishnu", "age":"21", "location":"kochi"} 
+# print(data.keys())
+# print(data.values())
+
+# for i in data:
+#     print(i,data[i])
+
+
+#reverse the string without using ::-1
+
+# a = "mohandas"
+# rev = ""
+# for i in range(len(a)-1,-1,-1):
+#     rev=rev+a[i]
+
+# print(end="")
+# print(rev)
+
+
+#remove the white space of the string
+
+data = "my name is mohan, i am 27 years old"
+# a = ""
+# for i in data:
+#     if i!= " ":
+#         a=a+i
+
+# print(a)
+
+
+data = {"name":"vishnudev","age":21,"location":"kochi"}
+print(data)
+print(data["name"])
+data["name"]="anjnana"
+print(data)
+data.pop("location")
+print(data)
+print(data.keys())
+print(data.values())
+
+
+
+
+ 
+
+
+
+
+
+
+
